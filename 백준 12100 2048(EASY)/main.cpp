b6a89d3ca90c42640d09660035380133c5f7fda9
dlwnsdyd94 <ljy1237@yonsei.ac.kr>
2021-04-15 22:47:04 +0900

    백준 12100 2048(EASY) 수정

--- 백준 12100 2048(EASY)/main.cpp
+++ 백준 12100 2048(EASY)/main.cpp
@@ -1,97 +1,132 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n, ret;
+#define MAX 20
 
-struct BOARD {
-    int map[20][20];
+int N;
+int ans;
 
-    void rotate() {
-        int temp[20][20];
+struct Board
+{
+	int myMap[MAX][MAX];
 
-        for (int y = 0; y < n; ++y) {
-            for (int x = 0; x < n; ++x) {
-                temp[y][x] = map[n - x - 1][y];
-            }
-        }
+	void rotate()
+	{
+		int temp[MAX][MAX];
+		for (int y = 0; y < N; y++)
+		{
+			for (int x = 0; x < N; x++)
+			{
+				temp[y][x] = myMap[N - x - 1][y];
+			}
+		}
 
-        for (int y = 0; y < n; ++y) {
-            for (int x = 0; x < n; ++x) {
-                map[y][x] = temp[y][x];
-            }
-        }
-    }
+		for (int y = 0; y < N; y++)
+		{
+			for (int x = 0; x < N; x++)
+			{
+				myMap[y][x] = temp[y][x];
+			}
+		}
+	}
 
-    int get_max() {
-        int ret = 0;
-        for (int y = 0; y < n; ++y) {
-            for (int x = 0; x < n; ++x) {
-                if (ret < map[y][x]) {
-                    ret = map[y][x];
-                }
-            }
-        }
-        return ret;
-    }
+	int get_max()
+	{
+		int myMax = 0;
+		for (int y = 0; y < N; y++)
+		{
+			for (int x = 0; x < N; x++)
+			{
+				if (myMax < myMap[y][x])
+				{
+					myMax = myMap[y][x];
+				}
+			}
+		}
+		return myMax;
+	}
 
-    void up() {
-        int temp[20][20];
+	void up()
+	{
+		int temp[MAX][MAX];
+		for (int x = 0; x < N; x++)
+		{
+			int flag = 0;
+			int target = -1;
+			for (int y = 0; y < N; y++)
+			{
+				if (myMap[y][x] == 0) continue;
+				if (flag == 1 && myMap[y][x] == temp[target][x])
+				{
+					temp[target][x] *= 2;
+					flag = 0;
+				}
+				else
+				{
+					temp[++target][x] = myMap[y][x];
+					flag = 1;
+				}
+			}
 
-        for (int x = 0; x < n; ++x) {
-            int flag = 0, target = -1;
-            for (int y = 0; y < n; ++y) {
-                if (map[y][x] == 0) {
-                    continue;
-                }
-                if (flag == 1 && map[y][x] == temp[target][x]) {
-                    temp[target][x] *= 2, flag = 0;
-                }
-                else {
-                    temp[++target][x] = map[y][x], flag = 1;
-                }
-            }
-            for (++target; target < n; ++target) {
-                temp[target][x] = 0;
-            }
-        }
-        for (int y = 0; y < n; ++y) {
-            for (int x = 0; x < n; ++x) {
-                map[y][x] = temp[y][x];
-            }
-        }
-    }
+			for (++target; target < N; ++target)
+			{
+				temp[target][x] = 0;
+			}
+		}
+
+		for (int y = 0; y < N; y++)
+		{
+			for (int x = 0; x < N; x++)
+			{
+				myMap[y][x] = temp[y][x];
+			}
+		}
+	}
 };
 
-void dfs(BOARD cur, int count) {
-    if (count == 5) {
-        int candi = cur.get_max();
-        if (ret < candi) {
-            ret = candi;
-        }
-        return;
-    }
+void dfs(Board cur, int count)
+{
+	if (count == 5)
+	{
+		int myMax = cur.get_max();
+		if (ans < myMax)
+		{
+			ans = myMax;
+		}
+		return;
+	}
 
-    for (int dir = 0; dir < 4; ++dir) {
-        BOARD next = cur;
-        next.up();
-        dfs(next, count + 1);
-        cur.rotate();
-    }
+	for (int dir = 0; dir < 4; dir++)
+	{
+		Board next = cur;
+		next.up();
+		dfs(next, count + 1);
+		cur.rotate();
+	}
 }
 
 int main()
 {
-    BOARD board;
-    cin >> n;
-    for (int y = 0; y < n; ++y) {
-        for (int x = 0; x < n; ++x) {
-            cin >> board.map[y][x];
-        }
-    }
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    ret = 0;
-    dfs(board, 0);
-    printf("%d\n", ret);
+	ifstream readFile;
+	readFile.open("input.txt");
+	readFile >> N;
 
-    return 0;
+	Board start;
+	for (int y = 0; y < N; y++)
+	{
+		for (int x = 0; x < N; x++)
+		{
+			readFile >> start.myMap[y][x];
+		}
+	}
+
+	ans = 0;
+	dfs(start, 0);
+	cout << ans;
+
+	return 0;
 }
