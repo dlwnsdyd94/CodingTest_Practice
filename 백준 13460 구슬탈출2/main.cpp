3c091facecf74b4e285500ac3d6ba7b1181c7b80
dlwnsdyd94 <ljy1237@yonsei.ac.kr>
2021-04-15 14:21:17 +0900

    백준 13460 구슬탈출2 수정본

--- 백준 13460 구슬탈출2/main.cpp
+++ 백준 13460 구슬탈출2/main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int N, M;
 char myMap[MAX][MAX];
-int visited[MAX][MAX][MAX][MAX];
 struct Location
 {
-	int rx, ry, bx, by, count;
+	int ry, rx, by, bx, count;
 };
-
-//상하좌우
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {-1, 1, 0, 0};
+int visited[MAX][MAX][MAX][MAX];
 
 int bfs(Location start)
 {
+	// 상하좌우
+	int dy[4] = {-1, 1, 0, 0};
+	int dx[4] = {0, 0, -1, 1};
+
 	queue<Location> q;
 	q.push(start);
 	visited[start.ry][start.rx][start.by][start.bx] = 1;
@@ -27,79 +27,83 @@ int bfs(Location start)
 		q.pop();
 
 		if (now_location.count > 10) break;
-		if(myMap[now_location.ry][now_location.rx] == 'O' && myMap[now_location.by][now_location.bx] != 'O')
+		if (myMap[now_location.ry][now_location.rx] == 'O' && myMap[now_location.by][now_location.bx] != 'O')
 		{
 			ans = now_location.count;
 			break;
 		}
 
-		for (int i = 0; i < 4; i++)
+		for (int dir = 0; dir < 4; dir++)
 		{
-			int new_rx = now_location.rx;
 			int new_ry = now_location.ry;
-			int new_bx = now_location.bx;
+			int new_rx = now_location.rx;
 			int new_by = now_location.by;
+			int new_bx = now_location.bx;
 
-			//R의 입장
 			while (1)
 			{
-				if (myMap[new_ry][new_rx] != '#' && myMap[new_ry][new_rx] != 'O')
+				if (myMap[new_ry][new_rx] != 'O' && myMap[new_ry][new_rx] == '#')
 				{
-					new_ry += dy[i];
-					new_rx += dx[i];
+					new_ry -= dy[dir];
+					new_rx -= dx[dir];
+					break;
 				}
 				else
 				{
-					if (myMap[new_ry][new_rx] == '#')
+					if (myMap[new_ry][new_rx] == 'O')
 					{
-						new_ry -= dy[i];
-						new_rx -= dx[i];
+						break;
 					}
-					break;
+					else
+					{
+						new_ry += dy[dir];
+						new_rx += dx[dir];
+					}
 				}
 			}
 
-			//B의 입장
 			while (1)
 			{
-				if (myMap[new_by][new_bx] != '#' && myMap[new_by][new_bx] != 'O')
+				if (myMap[new_by][new_bx] != 'O' && myMap[new_by][new_bx] == '#')
 				{
-					new_by += dy[i];
-					new_bx += dx[i];
+					new_by -= dy[dir];
+					new_bx -= dx[dir];
+					break;
 				}
 				else
 				{
-					if (myMap[new_by][new_bx] == '#')
+					if (myMap[new_by][new_bx] == 'O')
 					{
-						new_by -= dy[i];
-						new_bx -= dx[i];
+						break;
 					}
-					break;
+					else
+					{
+						new_by += dy[dir];
+						new_bx += dx[dir];
+					}
 				}
 			}
-
 			if (new_ry == new_by && new_rx == new_bx)
 			{
-				if (myMap[new_ry][new_rx] != 'O')
+				if (myMap[new_by][new_bx] != 'O')
 				{
 					int dist_r = abs(new_ry - now_location.ry) + abs(new_rx - now_location.rx);
 					int dist_b = abs(new_by - now_location.by) + abs(new_bx - now_location.bx);
 
 					if (dist_r > dist_b)
 					{
-						new_ry -= dy[i];
-						new_rx -= dx[i];
+						new_ry -= dy[dir];
+						new_rx -= dx[dir];
 					}
 					else
 					{
-						new_by -= dy[i];
-						new_bx -= dx[i];
+						new_by -= dy[dir];
+						new_bx -= dx[dir];
 					}
 				}
 			}
 
-			//push
-			if (visited[new_ry][new_rx][new_by][new_bx] == 0)
+			if(visited[new_ry][new_rx][new_by][new_bx] == 0)
 			{
 				visited[new_ry][new_rx][new_by][new_bx] = 1;
 				Location next;
